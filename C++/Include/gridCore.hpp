5ca0c16132432132d1945632d1d86d3e802c04e2
Shivam Mandloi <shivammandloi1102@gmail.com>
2026-02-22 01:23:01 +0530

    add fetch code

--- C++/Include/gridCore.hpp
+++ C++/Include/gridCore.hpp
@@ -5,26 +5,23 @@
 template <typename T>
 class gridCore
 {
-T* arr;
-int* stride;
-int *shape;
-int dim;
-int size;
+    T *arr;
+    size_t size, dim;
+    size_t *stride, *shape;
+
 public:
-    gridCore(std::initializer_list<int> list, T val) : size(1), dim(list.size())
+    gridCore(std::initializer_list<int> list, T val) : dim(list.size())
     {
-        stride = new int[list.size()];
-        shape = new int[list.size()];
+        stride = new size_t[list.size()];
+        shape = new size_t[list.size()];
 
         std::copy(list.begin(), list.end(), shape);
 
         // Initialize stride
-        int temp = 1;
-        for(int i = 0; i < dim; i++)
+        size = 1;
+        for (int i = dim - 1; i >= 0; --i)
         {
-            // stride is number of jumps from one dim to next
-            stride[dim - i - 1] = temp; 
-            temp *= shape[dim - i - 1];
+            stride[i] = size;
             size *= shape[i];
         }
 
@@ -32,23 +29,92 @@ public:
         std::fill(arr, arr + size, val);
     }
 
-    ~gridCore()
+    ~gridCore() noexcept
     {
         delete[] arr;
         delete[] stride;
         delete[] shape;
     }
 
+    gridCore(const gridCore<T> &other)
+    {
+        dim = other.dim;
+        size = other.size;
+
+        arr = new T[size];
+        stride = new size_t[dim];
+        shape = new size_t[dim];
+
+        std::copy(other.arr, other.arr + size, arr);
+        std::copy(other.stride, other.stride + dim, stride);
+        std::copy(other.shape, other.shape + dim, shape);
+    }
+
+    // This function move one grid to other gird
+    // This function does not copy
+    gridCore<T> &operator=(gridCore<T> &other)
+    {
+        if (this != &other)
+        {
+            delete[] arr;
+            delete[] stride;
+            delete[] shape;
+
+            shape = other.shape;
+            arr = other.arr;
+            stride = other.stride;
+            dim = other.dim;
+            size = other.size;
+
+            other.arr = nullptr;
+            other.shape = nullptr;
+            other.stride = nullptr;
+            other.dim = 0;
+            other.size = 0;
+        }
+        return *this;
+    }
+
+    int GetIndex(std::initializer_list<int> &list)
+    {
+        if (list.size() != dim)
+        {
+            std::cerr << "Dim misMatch got " << std::endl;
+            exit(0);
+        }
+        int index = 0, i = 0;
+        for (int val : list)
+        {
+            index += (val * stride[i]);
+            i += 1;
+        }
+        return index;
+    }
+
+    inline T &operator[](int index) { return arr[index]; }
+
+    template <typename... Args>
+    inline T &operator()(Args... args)
+    {
+        int indices[] = {args...};
+        int index = 0;
+        for (int i = 0; i < dim; ++i)
+        {
+            index += indices[i] * stride[i];
+        }
+        return arr[index];
+    }
+
     void Print()
     {
         std::cout << size << " " << dim << std::endl;
-        for(int i = 0; i < dim; i++)
+        for (int i = 0; i < dim; i++)
             std::cout << stride[i] << " ";
         std::cout << std::endl;
-        for(int i = 0; i < dim; i++)
+        for (int i = 0; i < dim; i++)
             std::cout << shape[i] << " ";
         std::cout << std::endl;
-        for(int i = 0; i < size; i++)
+        for (int i = 0; i < size; i++)
             std::cout << arr[i] << " ";
         std::cout << std::endl;
     }
--- C++/main.cpp
+++ C++/main.cpp
@@ -6,5 +6,11 @@ int main()
 {
     gridCore<int> gdCore({2, 2, 3}, 4);
     gdCore.Print();
+    std::cout << gdCore(0, 0, 1) << std::endl;
+
+    gridCore<int> anotherOne(gdCore);
+    anotherOne = gdCore;
+    std::cout << anotherOne(0, 0, 1) << std::endl;
+    std::cout << gdCore.size << " " << gdCore.dim << std::endl;
 
 }

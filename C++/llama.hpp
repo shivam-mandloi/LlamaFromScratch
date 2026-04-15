24952580601e7e97f4b007b71fab4a31b2217e51
Shivam Mandloi <shivammandloi1102@gmail.com>
2026-04-15 12:28:27 +0530

    add ROPE in C++

--- C++/llama.hpp
+++ C++/llama.hpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <cmath>
 #include <ATen/ATen.h>
 
 #include "HelpingFunction.hpp"
@@ -97,7 +98,9 @@ public:
         for (int i = 0; i < splitedText.size(); i++)
             res[i + 1] = vocabMap[splitedText[i]];
 
-        std::vector<int64_t> sizeOfEncodeVector = {static_cast<int64_t>(res.size()), };
+        std::vector<int64_t> sizeOfEncodeVector = {
+            static_cast<int64_t>(res.size()),
+        };
         at::Tensor encRes = at::from_blob(res.data(), sizeOfEncodeVector, at::kInt).clone();
         return encRes;
     }
@@ -106,7 +109,7 @@ public:
     {
         std::string decdString = "";
         for (int i = 0; i < encd.size(0); i++)
-            decdString += vocabArr[encd[i].item<int>()];        
+            decdString += vocabArr[encd[i].item<int>()];
 
         size_t pos;
 
@@ -121,13 +124,14 @@ public:
 
 class Attention
 {
-at::Tensor query, key, value, output;
+    at::Tensor query, key, value, output, kCache, vCache;
+    int qkvDim = 128;
 
 public:
-    Attention(int index, std::vector<int64_t> querySize, 
-        std::vector<int64_t> keySize, 
-        std::vector<int64_t> valueSize,
-        std::vector<int64_t> outputSize)
+    Attention(int index, std::vector<int64_t> querySize,
+              std::vector<int64_t> keySize,
+              std::vector<int64_t> valueSize,
+              std::vector<int64_t> outputSize)
     {
         // Load Parameters
         query = LoadTensor("model.layers." + std::to_string(index) + ".self_attn.q_proj.weight.bin", querySize);
@@ -136,23 +140,87 @@ public:
         output = LoadTensor("model.layers." + std::to_string(index) + ".self_attn.o_proj.weight.bin", outputSize);
     }
 
-    void forward()
+    void forward(at::Tensor x,
+                 at::Tensor mask,
+                 std::vector<float> &sinTheta,
+                 std::vector<float> &cosTheta,
+                 int seqLen)
     {
+        /*
+            x: (b X n)
+            ropeTheta: (qkvDim / 2)
+            sinTheta: virtualy size (seqLen X (qkvDim/2))
+            cosTheta: virtualy size (seqLen X (qkvDim/2))
+            seqlen: sequence size
 
+            For sinTheta and cosTheta have higher or equal size to sequence length
+        */
+
+        // Get the bath, seqLen and dim info
+        int64_t b = x.size(0), n = x.size(2);
+
+        // Dim: b X (head * qkvDim)
+        at::Tensor q = x.matmul(query.t());
+        at::Tensor k = x.matmul(key.t());
+        at::Tensor v = x.matmul(value.t());
+
+        q = q.view({b, 24, 128});
+        v = v.view({b, 8, 128});
+        k = k.view({b, 8, 128});
+
+        /*
+            ROPE Implementation
+        */
+
+        // Dim: seq X (qkvDim/2)
+        // generate the last sin and cos tensor from the vector
+        at::Tensor sin = at::from_blob(sinTheta.data() + static_cast<int>((seqLen * (qkvDim / 2))), {static_cast<int64_t>(qkvDim / 2)}).unsqueeze(0).unsqueeze(0);
+        at::Tensor cos = at::from_blob(cosTheta.data() + static_cast<int>((seqLen * (qkvDim / 2))), {static_cast<int64_t>(qkvDim / 2)}).unsqueeze(0).unsqueeze(0);
+        
+        // Generate the sin and cos value for the query
+        at::Tensor tSin = sin.repeat({b, 24, 1});
+        at::Tensor tCos = cos.repeat({b, 24, 1});
+
+
+        q = at::cat({
+            q.slice(-1, 0, static_cast<int>(seqLen/2)) * tCos - q.slice(-1, static_cast<int>(seqLen/2), seqLen) * tSin,
+            q.slice(-1, 0, static_cast<int>(seqLen/2)) * tSin + q.slice(-1, static_cast<int>(seqLen/2), seqLen) * tCos
+        }, -1);
+
+
+        // Generate the sin and cos value for the key
+        
+        tSin = sin.repeat({b, 8, 1});
+        tCos = cos.repeat({b, 8, 1});
+
+        q = at::cat({
+            k.slice(-1, 0, static_cast<int>(seqLen/2)) * tCos - k.slice(-1, static_cast<int>(seqLen/2), seqLen) * tSin,
+            k.slice(-1, 0, static_cast<int>(seqLen/2)) * tSin + k.slice(-1, static_cast<int>(seqLen/2), seqLen) * tCos
+        }, -1);
     }
 };
 
 class MLP
 {
-
 };
 
 class Transfomer
 {
-
 };
 
 class llamma
 {
+    at::Tensor theta;
+    int qkvDim = 128;
+    float ropeTheta = 500000.0;
 
+public:
+    llamma()
+    {
+        std::vector<float> thetaData(qkvDim / 2, 0);
+        for (int i = 0; i < thetaData.size(); i++)
+            thetaData[i] = pow(ropeTheta, -2 * (i) / qkvDim);
+
+        theta = at::from_blob(thetaData.data(), {static_cast<int64_t>(qkvDim / 2),}).clone();
+    }
 };
